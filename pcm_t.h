30a3dacb701b52b74da9765711f918062db9e1d6
Wojciech Przytuła <wp418383@students.mimuw.edu.pl>
2022-01-05 10:47:30 +0100

    Still working, on track to find the bug

--- pcm_t.h
+++ pcm_t.h
@@ -1,8 +1,14 @@
-//
-// Created by xps15 on 04.01.2022.
-//
-
 #ifndef DAC_PCM_T_H
 #define DAC_PCM_T_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+    size_t sample_rate;
+    size_t bytes_per_sample;
+    size_t data_len;
+    uint8_t const* data;
+} pcm_t;
 
 #endif //DAC_PCM_T_H
